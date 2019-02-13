3b43673d64ef84dc024728460df776ec501329a3
Patrick Martin <patrickmartin88@gmail.com>
2019-02-13 15:22:52 -0800

    Initial Commit

---
+++ src/main/java/com/lambdaschool/javacities/JavaCitiesApplication.java
@@ -0,0 +1,14 @@
+package com.lambdaschool.javacities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCitiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCitiesApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/lambdaschool/javacities/JavaCitiesApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lambdaschool.javacities;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaCitiesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
