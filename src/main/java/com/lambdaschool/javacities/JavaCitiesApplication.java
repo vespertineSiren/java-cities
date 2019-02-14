79c2acb72530c6c1b8eddaf7ea673939a5ec9ce4
Patrick Martin <patrickmartin88@gmail.com>
2019-02-13 16:09:17 -0800

    Added Constants to JavaCitiesApplication before moving forward.

--- src/main/java/com/lambdaschool/javacities/JavaCitiesApplication.java
+++ src/main/java/com/lambdaschool/javacities/JavaCitiesApplication.java
@@ -2,10 +2,17 @@ package com.lambdaschool.javacities;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class JavaCitiesApplication {
 
+    public static final String EXCHANGE_NAME = "LambdaServer";
+    public static final String QUEUE_NAME_CITIES1 = "Cities1PriorityQueue";
+    public static final String QUEUE_NAME_CITIES2 = "Cities2PriorityQueue";
+    public static final String QUEUE_NAME_SECRET = "SecretPriorityQueue";
+    
     public static void main(String[] args) {
         SpringApplication.run(JavaCitiesApplication.class, args);
     }
