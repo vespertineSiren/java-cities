8e8249829502bc591f51a716b74a29185599dca6
Patrick Martin <patrickmartin88@gmail.com>
2019-02-14 10:58:31 -0800

    Finished up controller.

--- src/main/java/com/lambdaschool/javacities/CityMessageListener.java
+++ src/main/java/com/lambdaschool/javacities/CityMessageListener.java
@@ -7,16 +7,19 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class CityMessageListener {
+
     @RabbitListener(queues = JavaCitiesApplication.QUEUE_NAME_SECRET)
     public void receiveSecretMessage(CityMessage cm)
     {
         log.info("Received Secret Message: {} ", cm.toString());
     }
+
     @RabbitListener(queues = JavaCitiesApplication.QUEUE_NAME_CITY1)
     public void receiveCity1Message(CityMessage cm)
     {
         log.info("Received City1 Message: {} ", cm.toString());
     }
+
     @RabbitListener(queues = JavaCitiesApplication.QUEUE_NAME_CITY2)
     public void receiveCity2Message(CityMessage cm)
     {
